6db2ec1818bd3d982a05ea6fba5ab8edb5c42660
theGeekkit <sonofgondolin16@gmail.com>
2022-10-08 15:12:31 -0500

    10-8-22

--- src/app/heroes-list/heroes-list.component.ts
+++ src/app/heroes-list/heroes-list.component.ts
@@ -10,20 +10,20 @@ export class HeroesListComponent implements OnInit {
 
   hero: Hero[] = [
     {
-      name:"",
-      imageUrl:""
+      name:"Alexis Polux",
+      imageUrl:"https://i.postimg.cc/9fb1pdmH/Alexis-Pollux-portrait.jpg"
     },
     {
-      name:"",
-      imageUrl:""
+      name:"Fafnir Rann",
+      imageUrl:"https://i.postimg.cc/RVjnkNs6/IF-warlord-Fafnir-portrait.jpg"
     },
     {
-      name:"",
-      imageUrl:""
+      name:"Sigismund",
+      imageUrl:"https://i.postimg.cc/G2vnMg5S/sigismund-portrait.jpg"
     },
     {
-      name:"",
-      imageUrl:""
+      name:"Tor Garadon",
+      imageUrl:"https://i.postimg.cc/1RLJR546/Tor-Garadon-02-650x650.jpg"
     },
   ]
 
