a518d0e2cef2a386295c5a7bf3d0f26f7606e5ac
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-13 19:21:13 -0500

    10-13-22

--- src/app/heroes-list/heroes-list.component.ts
+++ src/app/heroes-list/heroes-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Model/hero';
 import { Router } from '@angular/router';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes-list',
@@ -9,51 +10,16 @@ import { Router } from '@angular/router';
 })
 export class HeroesListComponent implements OnInit {
 
-  hero: Hero[] = [
-    {
-      id: 1,
-      name:"Alexis Polux",
-      imageUrl:"https://i.postimg.cc/9fb1pdmH/Alexis-Pollux-portrait.jpg",
-      bigImageUrl:'',
-      description:''
-    },
-    {
-      id: 2,
-      name:"Fafnir Rann",
-      imageUrl:"https://i.postimg.cc/RVjnkNs6/IF-warlord-Fafnir-portrait.jpg",
-      bigImageUrl:'',
-      description:''
-    },
-    {
-      id: 3,
-      name:"Sigismund",
-      imageUrl:"https://i.postimg.cc/G2vnMg5S/sigismund-portrait.jpg",
-      bigImageUrl:'',
-      description:''
-    },
-    {
-      id: 4,
-      name:"Tor Garadon",
-      imageUrl:"https://i.postimg.cc/1RLJR546/Tor-Garadon-02-650x650.jpg",
-      bigImageUrl:'',
-      description:''
-    },
-    {
-      id: 5,
-      name:"Dorn",
-      imageUrl:"",
-      bigImageUrl:'',
-      description:''
-    },
-  ]
+  hero: Hero[] = [];
 
-  constructor(private router: Router) { }
+
+  constructor(public heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroService.heroes$(heroes => {
+      this.heroes = heroes
+    })
   }
 
-gotoList() {
-  this.router.navigate(['/src/app/app.component.html'])
-}
 
 }
