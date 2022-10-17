e376e66f8d92ed2c52b7e67047f4eeed493330d1
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-10-17 18:57:11 -0500

    10-17-22

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,43 +7,43 @@ import { Hero } from "./Model/hero";
   //   this.heroes = heroes
   // })
 
-  loadHeroes() {
-    this.heroes.next([
-      {
-        id: 1,
-        name:"Alexis Polux",
-        imageUrl:"https://i.postimg.cc/9fb1pdmH/Alexis-Pollux-portrait.jpg",
-        bigImageUrl:'',
-        description:''
-      },
-      {
-        id: 2,
-        name:"Fafnir Rann",
-        imageUrl:"https://i.postimg.cc/RVjnkNs6/IF-warlord-Fafnir-portrait.jpg",
-        bigImageUrl:'',
-        description:''
-      },
-      {
-        id: 3,
-        name:"Sigismund",
-        imageUrl:"https://i.postimg.cc/G2vnMg5S/sigismund-portrait.jpg",
-        bigImageUrl:'',
-        description:''
-      },
-      {
-        id: 4,
-        name:"Tor Garadon",
-        imageUrl:"https://i.postimg.cc/1RLJR546/Tor-Garadon-02-650x650.jpg",
-        bigImageUrl:'',
-        description:''
-      },
-      {
-        id: 5,
-        name:"Dorn",
-        imageUrl:"",
-        bigImageUrl:'',
-        description:''
-      },
-    ])
-  }
+loadHeroes() {
+  this.heroes.next([
+    {
+      id: 1,
+      name:"Alexis Polux",
+      imageUrl:"https://i.postimg.cc/9fb1pdmH/Alexis-Pollux-portrait.jpg",
+      bigImageUrl:'',
+      description:''
+    },
+    {
+      id: 2,
+      name:"Fafnir Rann",
+      imageUrl:"https://i.postimg.cc/RVjnkNs6/IF-warlord-Fafnir-portrait.jpg",
+      bigImageUrl:'',
+      description:''
+    },
+    {
+      id: 3,
+      name:"Sigismund",
+      imageUrl:"https://i.postimg.cc/G2vnMg5S/sigismund-portrait.jpg",
+      bigImageUrl:'',
+      description:''
+    },
+    {
+      id: 4,
+      name:"Tor Garadon",
+      imageUrl:"https://i.postimg.cc/1RLJR546/Tor-Garadon-02-650x650.jpg",
+      bigImageUrl:'',
+      description:''
+    },
+    {
+      id: 5,
+      name:"Dorn",
+      imageUrl:"",
+      bigImageUrl:'',
+      description:''
+    },
+  ])
+}
 
